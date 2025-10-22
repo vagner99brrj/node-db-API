cd6d8c7662a51e35dac38f6d876e6ae13c392cb4
Vagner Bruno <vagnerbruno0699@gmail.com>
2025-10-22 14:14:03 -0300

    Adiciona rotas GET e POST para gestão de usuários

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+
 import express from 'express';
 
+//iniciando o express
 const app = express();
 
+app.use(express.json());
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    users.push(req.body);
+
+    res.send('OK, funcionou!');
+
+});
+
 app.get('/usuarios', (req, res) => {
-  res.send('OK, funcionou!');
+
+  res.json(users);
+
 });
 
 
